cae33b023f06dde509b41063aef9b78d8dace57c
Tejas Patil <135954933+tejasp08@users.noreply.github.com>
2025-08-29 14:46:52 +0530

    Add files via upload

---
+++ Day1.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Day1{
+    public static void main(String[] args) {
+        //Hello world
+        // System.out.print("Hello World!!");
+        // System.out.println("My name is xyz");
+
+        //Using scanner for taking input with string and int
+        // Scanner input = new Scanner(System.in);
+        // System.out.println("What is your name ?");
+        // String name = input.next();
+        // System.out.println("My name is " + name);
+
+        // Scanner input = new Scanner(System.in);
+        // System.out.println("What is your Rollno ?");
+        // int roll = input.nextInt();
+        // System.out.println("My rollno. is " + roll);
+
+        //Basic arthmethic operators
+        // Scanner input1 = new Scanner(System.in);
+        // System.out.println("Enter number 1: ");
+        // int num1 = input1.nextInt();
+        // Scanner input2 = new Scanner(System.in);
+        // System.out.println("Enter number 2: ");
+        // int num2 = input1.nextInt();
+        // System.out.println("The addition of two numbers is: " + (num1 + num2));
+        // System.out.println("The subtraction of two numbers is: " + (num1 - num2));
+        // System.out.println("The multiplication of two numbers is: " + (num1 * num2));
+        // System.out.println("The division of two numbers is: " + (num1 / num2));
+
+        //Simple area of circle 
+        // Scanner input = new Scanner(System.in);
+        // System.out.println("What is the radius of circle: ");
+        // float r = input.nextFloat();
+        // double area = 3.14159 * r * r;
+        // System.out.println("the area of circle is " + area + '.');
+
+    }
+
+}
