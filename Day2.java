4b9a06873c6df0c1a2784c1a2e665081df02666c
tejasp08 <tejasp482003@gmail.com>
2025-08-30 11:32:32 +0530

    Add Day2 file

--- Day2.java
+++ Day2.java
@@ -1,3 +1,23 @@
 public class Day2 {
-    
+    public static void main(String[] args) {
+        
+        //Conditionals if-else
+        int salary = 25000;
+        if (salary>10000) {
+            salary = salary + 2000;
+        } else {
+            salary = salary + 1000;
+        }
+        System.out.println("the new salary is " + salary);
+
+        //Multiple if-else
+        if (salary > 10000) {
+            salary += 2000;
+        } else if (salary > 20000) {
+            salary += 3000;
+        } else {
+            salary += 1000;
+        }
+        System.out.println("the new salary is " + salary);
+    }
 }
