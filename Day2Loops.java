14cab03428abf7b670d9eb09011e79e9c088d456
tejasp08 <tejasp482003@gmail.com>
2025-08-30 11:35:39 +0530

    Add Loops file

---
+++ Day2Loops.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Day2Loops {
+    public static void main(String[] args) {
+        
+        //Printing numbers from 1 to 5 using for loop
+        // for (int num = 1; num <= 5; num++) {
+        //     System.out.println(num);
+        // }
+
+        //Printing numbers from 1 to n using for loop
+        // Scanner input = new Scanner(System.in);
+        // System.out.println("Enter a number: ");
+        // int n = input.nextInt();
+        // System.out.println("The sequence till " + n + " is:");
+        // for (int num = 1; num <= n; num++) {
+        //     System.out.print(num + " ");
+        // }
+
+        //Printing numbers from 1 to 5 using while loop
+        // int num = 1;
+        // while (num <= 5) {
+        //     System.out.print(num + " ");
+        //     num++;
+        // }
+
+        //Printing numbers from 1 to n using while loop
+        // Scanner input = new Scanner(System.in);
+        // System.out.println("Enter a number: ");
+        // int n = input.nextInt();
+        // int num = 1;
+        // while (num <= n) {
+        //     System.out.print(num + " ");
+        //     num++;
+        // }
+
+        //Printing numbers using do while loop
+        // int num = 1;
+        // do {
+        //     System.out.print(num + " ");
+        //     num++;
+        // } while (num <= 10);
+    }
+}
