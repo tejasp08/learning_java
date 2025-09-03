4c6aa45e12e4a7298b9073a1f2177dd792e4f5e0
tejasp08 <tejasp482003@gmail.com>
2025-09-03 13:16:57 +0530

    Add the file

---
+++ SwitchCase.java
@@ -0,0 +1,82 @@
+import java.util.Scanner;
+
+public class SwitchCase {
+    public static void main(String[] args) {
+        
+        Scanner in = new Scanner(System.in);
+        // System.out.println("Enter a name of strawhat crew: ");
+        // String name = in.next();
+
+        // switch (name) {
+        //     case "luffy":
+        //         System.out.println("Captain");
+        //         break;
+        //     case "zoro":
+        //         System.out.println("Swordsman");
+        //         break;
+        //     case "nami":
+        //         System.out.println("Navigator");
+        //         break;
+        //     case "sanji":
+        //         System.out.println("Chef");
+        //         break;
+        //     case "ussop":
+        //         System.out.println("Sniper");
+        //         break;
+        //     case "chopper":
+        //         System.out.println("Doctor");
+        //         break;
+        //     case "robin":
+        //         System.out.println("Archeologist");
+        //         break;
+        //     case "franky":
+        //         System.out.println("Shipwright");
+        //         break;
+        //     case "brook":
+        //         System.out.println("Singer/Swordsman");
+        //         break;
+        //     case "jimbei":
+        //         System.out.println("Helmsman");
+        //         break;
+        //     default:
+        //         System.out.println("Invalid name!!!");
+        //         break;
+        // }
+
+
+        // System.out.println("Enter a day number");
+        // int day = in.nextInt();
+        //New or enhanced syntax
+        // switch (day) {
+        //     case 1 -> System.out.println("Monday");
+        //     case 2 -> System.out.println("Tuesday");
+        //     case 3 -> System.out.println("Wednesday");
+        //     case 4 -> System.out.println("Thursday");
+        //     case 5 -> System.out.println("Friday");
+        //     case 6 -> System.out.println("Saturday");
+        //     case 7 -> System.out.println("Sunday");
+        //     default -> System.err.println("invalid number");
+        // }
+
+
+        //Multiple switch case
+        // System.out.println("Enter Employee no. and department");
+        // int empno = in.nextInt();
+        // String dept = in.next();
+
+        // switch(empno) {
+        //     case 1 -> System.out.println("John Doe");
+        //     case 2 -> System.out.println("William Doe");
+        //     case 3 -> System.out.println("John Cross");
+        //     case 4 -> {
+        //         System.out.println("Employee number 4");
+        //         switch(dept) {
+        //             case "IT" -> System.out.println("IT Department");
+        //             case "HR" -> System.out.println("HR Department");
+        //             default -> System.out.println("Inavlid Dept.");
+        //         }
+        //     }
+        //     default -> System.out.println("Invalid number");
+        // }
+    }
+}
