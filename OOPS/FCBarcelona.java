e169e6b273ef1cc36d952df5c66ced4f0263a7ea
tejasp08 <tejasp482003@gmail.com>
2025-10-02 00:32:21 +0530

    Add folder

---
+++ OOPS/FCBarcelona.java
@@ -0,0 +1,13 @@
+package OOPS;
+
+public class FCBarcelona {
+    String player_name;
+    String player_position;
+    String status;
+    @Override
+    public String toString() {
+        return "FCBarcelona [player_name = " + player_name + ", player_position = " + player_position + ", status = " + status
+                + "]";
+    }
+
+}
---
+++ OOPS/Test.java
@@ -0,0 +1,33 @@
+package OOPS;
+
+public class Test {
+    //Many classes will be called in this file
+
+    public static void main(String[] args) {
+    
+        //Calling FCBarcelona class
+        FCBarcelona raphina = new FCBarcelona();
+        raphina.player_name = "Raphina";
+        raphina.player_position = "LW/CAM";
+        raphina.status = "Active";
+        System.out.println(raphina);
+
+        FCBarcelona lamine = new FCBarcelona();
+        lamine.player_name = "Lamine Yamal";
+        lamine.player_position = "RW";
+        lamine.status = "Active";
+        System.out.println(lamine);
+
+        FCBarcelona pedri = new FCBarcelona();
+        pedri.player_name = "Pedri";
+        pedri.player_position = "CM";
+        pedri.status = "Active";
+        System.out.println(pedri);
+
+        FCBarcelona iniesta = new FCBarcelona();
+        iniesta.player_name = "Andres Iniesta";
+        iniesta.player_position = "CM";
+        iniesta.status = "Retired";
+        System.out.println(iniesta);
+    }
+}
