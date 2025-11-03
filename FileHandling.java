0a08abf54aa3fd3547b04ce83fa740ab85465e81
tejasp08 <tejasp482003@gmail.com>
2025-11-03 23:45:38 +0530

    Updated file

--- FileHandling.java
+++ FileHandling.java
@@ -1,5 +1,8 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.util.Scanner;
 
@@ -40,18 +43,49 @@ public class FileHandling {
         }
 
         //Writing in file
-        String write = "This is writing through fileWriter";
+        String write = "This is writing through fileWriter.";
+        String write2 = " Hello World!!!";
         try {
             //creating a writer using fileWriter
             FileWriter writeOut = new FileWriter("output.txt");
 
             //writing string to file
             writeOut.write(write);
+            writeOut.write(write2);
             System.out.println("Data is written");
 
             writeOut.close();
         } catch (Exception e) {
             e.getStackTrace();
+        }
+
+        //Buffered Writer
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter("Buffered.txt"))){
+            bw.write("Writing first line");
+            bw.newLine();   //line break
+            bw.write("Writing second line");
+            System.out.println("Successfully wrote in the text file");
+        } catch (Exception e) {
+            System.out.println("Error in writing file!");
+        }
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter("Buffered.txt", true))){
+            bw.newLine();
+            bw.write("Appended new line");
+            System.out.println("Successfully appended in the text file");
+        } catch (Exception e) {
+            System.out.println("Error writing file");
+        }
+
+        //Buffered reader
+        try (BufferedReader br = new BufferedReader(new FileReader("Buffered.txt"))){
+            System.out.println("Text in the file: ");
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (Exception e) {
+            System.out.println("Error in reading file");
         }
     }
 }
